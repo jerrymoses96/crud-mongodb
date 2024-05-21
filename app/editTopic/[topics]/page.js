1ce558e674b0f75e633c672b4004528dd5a42134
jerin <jerinkunnamkada@gmail.com>
2024-05-21 12:02:53 +0530

    final update

--- app/editTopic/[topics]/page.js
+++ app/editTopic/[topics]/page.js
@@ -1,25 +1,35 @@
 import EditTopicForm from "@/components/EditTopicForm";
 import React from "react";
 
+// Function to fetch topic by ID
 const getTopicById = async (id) => {
   try {
-    const res = fetch(`http://localhost:3000/api/topics/${id}`, {
+    const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
       cache: "no-store",
     });
     if (!res.ok) {
       throw new Error("Failed to fetch Topic");
     }
-    return (await res).json();
+    return await res.json();
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    return null;
   }
 };
 
+// Main component to display the EditTopicForm
 const Topics = async ({ params }) => {
   const { topics } = params;
   console.log(topics);
-  const { topic } = await getTopicById(topics);
-  const { title, description } = topic;
+
+  const Topic = await getTopicById(topics);
+
+  if (!Topic || !Topic.topic) {
+    return <div>Error loading topic.</div>;
+  }
+
+  const { title, description } = Topic.topic;
+
   return <EditTopicForm id={topics} title={title} description={description} />;
 };
 
--- components/EditTopicForm.jsx
+++ components/EditTopicForm.jsx
@@ -1,15 +1,32 @@
 "use client";
 import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const EditTopicForm = ({ id, title, description }) => {
   const [newTitle, setNewTitle] = useState(title);
   const [newDescription, setNewDescription] = useState(description);
-  const handleSubmit =(e)=>{
+  const router = useRouter();
+  const handleSubmit = async (e) => {
     e.preventDefault();
-
-  }
+    try {
+      const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ newTitle, newDescription }),
+      });
+      if (!res.ok) {
+        throw new Error("Failed to update topic");
+      }
+      router.push("/");
+      router.refresh();
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <form  onSubmit={handleSubmit} className="flex flex-col gap-3 ">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3 ">
       <input
         onChange={(e) => setNewTitle(e.target.value)}
         value={newTitle}
